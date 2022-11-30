224975aab2910d9f7b742b308f45d3709e25a47e
JaegeonRyu <worjswogn@hanmail.net>
2022-11-30 18:23:05 +0900

    총알 발사 위치 및 크기 수정

--- bullet.py
+++ bullet.py
@@ -16,32 +16,33 @@ BULLET_SPEED_PPS = BULLET_SPEED_MPS * PIXEL_PER_METER
 class Bullet:
     image = None
 
-    def __init__(self, x=400, y=300, x_velocity=1, y_velocity=1):
+    def __init__(self, x=0, y=0, x_velocity=1, y_velocity=1):
         if Bullet.image == None:
             Bullet.image = load_image('bullet.png')
         self.x, self.y, self.x_velocity, self.y_velocity = x, y, x_velocity, y_velocity
         self.bullet_frame = 0
         self.frame = 0
 
+    def return_xy(self, x, y, xd, yd):
+        self.x, self.y, self.x_velocity, self.y_velocity = x, y, xd, yd
+
     def draw(self):
         self.bullet_frame = (self.bullet_frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
 
         if self.x_velocity > 0:
             self.x += self.x_velocity * BULLET_SPEED_PPS * game_framework.frame_time
-            self.image.clip_draw(int(self.bullet_frame) * 100, 100, 100, 100, self.x, self.y)
+            self.image.clip_draw(int(self.bullet_frame) * 80, 80, 80, 80, self.x+15, self.y+15)
         if self.y_velocity > 0:
             self.y += self.y_velocity * BULLET_SPEED_PPS * game_framework.frame_time
-            self.image.clip_composite_draw(int(self.bullet_frame)*100, 100, 100, 100, 3.141592 / 2, '', self.x, self.y, 100, 100)
+            self.image.clip_composite_draw(int(self.bullet_frame)*80, 80, 80, 80, 3.141592 / 2, '', self.x, self.y, 80, 80)
         if self.x_velocity < 0:
             self.x += self.x_velocity * BULLET_SPEED_PPS * game_framework.frame_time
-            self.image.clip_composite_draw(int(self.bullet_frame)*100, 100, 100, 100, 3.141592, '', self.x, self.y, 100, 100)
+            self.image.clip_composite_draw(int(self.bullet_frame)*80, 80, 80, 80, 3.141592, '', self.x+15, self.y+15, 80, 80)
         if self.y_velocity < 0:
             self.y += self.y_velocity * BULLET_SPEED_PPS * game_framework.frame_time
-            self.image.clip_composite_draw(int(self.bullet_frame)*100, 100, 100, 100, -3.141592 / 2, '', self.x, self.y, 100, 100)
+            self.image.clip_composite_draw(int(self.bullet_frame)*80, 80, 80, 80, -3.141592 / 2, '', self.x, self.y, 80, 80)
 
-        # draw_rectangle(*self.get_bb())
-
-
+        draw_rectangle(*self.get_bb())
 
     def update(self):
         self.x += self.x_velocity
@@ -49,14 +50,14 @@ class Bullet:
 
         if self.x < 0 or self.x > 800:
             game_world.remove_object(self)
-        if self.y < 0 or self.y > 600:
+        if self.y < 0 or self.y > 550:
             game_world.remove_object(self)
 
     def get_bb(self):
         return self.x - 10, self.y - 5, self.x + 10, self.y + 5
 
     def handle_collision(self, other, group):
-        print('bullet meet mob')
         if group == 'bullet:mobs':
             game_world.remove_object(self)
+            pass
 
