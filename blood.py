bc361acef79ca0ee6bfc62fd7b07faba1b5e1e1f
JaegeonRyu <worjswogn@hanmail.net>
2022-12-06 00:05:39 +0900

    blood.py 추가 (좀비 피격)

--- blood.py
+++ blood.py
@@ -6,10 +6,11 @@ import server
 
 class Blood:
     image = None
-    def __init__(self, x=0, y=0):
+
+    def __init__(self, x, y):
         if Blood.image == None:
             Blood.image = load_image('resources\\blood_2.png')
-            self.x, self.y = x, y
+        self.x, self.y = x, y
 
     def draw(self):
         self.image.draw(self.x, self.y)
